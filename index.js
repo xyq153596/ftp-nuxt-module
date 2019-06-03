65e639b174c0af4848fb116c3e8838b67e31fb4a
junjun <zhangjun0911@pinpianyi.com>
2019-06-03 14:33:41 +0800

    update

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const fse = require("fs-extra");
 const node_ssh = require("node-ssh");
 const consola = require("consola");
 const chalk = require("chalk");
+const path = require("path");
 
 const ssh = new node_ssh();
 
 async function asyncModule() {
-  const { buildEnv } = this.options.global;
-  if (buildEnv !== "dev") {
+  const { BUILD_ENV, IS_BUILD_DEV } = this.options.env;
+  const { outputDir, dirName } = this.options.globalModuleOptions;
+  if (!IS_BUILD_DEV) {
     return;
   }
-  
+
   this.nuxt.hook("generate:done", async nuxt => {
-    const ftp_config = nuxt.options.project.ftp;
+    const ftp_config = nuxt.options.project.ftp[BUILD_ENV];
     const ssh_config = {
       host: ftp_config.host,
       username: ftp_config.username,
       password: ftp_config.password,
-      port: ftp_config.port
+      port: ftp_config.port,
+      readyTimeout: 60000
     };
-    const distDir = nuxt.options.generate.dir;
+    const distDir = path.join(nuxt.options.rootDir, "dist", outputDir, dirName);
     const isExist = await fse.pathExists(distDir);
     if (isExist) {
       try {
@@ -29,16 +32,14 @@ async function asyncModule() {
         return Promise.resolve();
       }
       //清空目标目录
-      await ssh.execCommand(
-        `rm -rf ${ftp_config.dir}/${nuxt.options.project.dirName}/`
-      );
+      //   await ssh.execCommand(
+      //     `rm -rf ${ftp_config.dir}/${nuxt.options.project.dirName}/`
+      //   );
       consola.info("正在将代码上传至测试服务器。。。");
       //上传代码
       const result = await ssh.putDirectory(
         distDir,
-        `${ftp_config.dir}/${nuxt.options.project.dirName}/${
-          nuxt.options.project.version.nextDev
-        }`,
+        `${ftp_config.dir}/${nuxt.options.project.dirName}`,
         {
           recursive: true,
           concurrency: 1,
@@ -54,7 +55,6 @@ async function asyncModule() {
       if (result) {
         consola.success(`上传代码成功!`);
         consola.info("最新测试版本", `${nuxt.options.project.version.nextDev}`);
-        consola.info("最新测试地址", `${nuxt.options.build.publicPath}`);
       } else {
         consola.error("上传失败，请重新上传");
       }
